6dbb2d82e08a7ba8a758fd1003a936320f78cba9
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-03-23 13:56:56 -0400

    backend setup

--- app/controllers/survivors_controller.rb
+++ app/controllers/survivors_controller.rb
@@ -1,2 +1,17 @@
 class SurvivorsController < ApplicationController
+    def index
+        survivors = Survivor.all 
+        render json: survivors
+    end
+    def show
+        survivor = Survivor.find(params[:id])
+        render json: survivor 
+    end
+    def create
+        survivor = Survivor.create(
+            tool_id: params[:tool_id],
+            shelter_id: params[:shelter_id],
+        )
+        render json: survivor
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Survivor.destroy_all
+Tools.destroy_all
+Shelter.destroy_all
+
+t1 = Tool.create(name: "Machete", rating: 8, image: "")
+t2 = Tool.create(name: "Duct Tape", rating: 6, image: "")
+t3 = Tool.create(name: "Toilet Paper", rating: 4, image: "")
+t4 = Tool.create(name: "Fire Starter", rating: 7, image: "")
+t5 = Tool.create(name: "Hand Sanitizer", rating: 5, image: "")
+t6 = Tool.create(name: "Hatchet", rating: 6, image: "")
+
+s1 = Shelter.create(name: "Tarp", rating: 6, image: "")
+s2 = Shelter.create(name: "Debris", rating: 5, image: "")
+s1 = Shelter.create(name: "Quarantine House", rating: 7, image: "")
+s1 = Shelter.create(name: "Cave", rating: 8, image: "")
+s1 = Shelter.create(name: "Igloo", rating: 8, image: "")
+s1 = Shelter.create(name: "Tree", rating: 4, image: "")
+
