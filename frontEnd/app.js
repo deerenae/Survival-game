992e138a058a6be1fd56e68edeecf5325c745e0d
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-03-24 16:58:38 -0400

    added to frontEnd and fixed seed variables

--- frontEnd/app.js
+++ frontEnd/app.js
@@ -1,3 +1,28 @@
+const shelterDropdown = document.querySelector('#shelter-dropdown')
+const toolDropdown = document.querySelector('#tool-dropdown')
+
 fetch('http://localhost:3000/shelters')
 .then(response => response.json())
-.then(console.log)
+.then(result => shelterList(result))
+
+fetch('http://localhost:3000/tools')
+.then(response => response.json())
+.then(result => toolList(result))
+
+function shelterList(shelters) {
+    shelters.forEach(shelter => {
+        const shelterOptions = document.createElement('option')
+        shelterOptions.innerText = shelter.name
+        shelterOptions.value = shelter.id
+        shelterDropdown.append(shelterOptions)
+    })
+}
+
+function toolList(tools) {
+    tools.forEach(tool => {
+        const toolOptions = document.createElement('option')
+        toolOptions.innerText = tool.name
+        toolOptions.value = tool.id
+        toolDropdown.append(toolOptions)
+    })
+}
