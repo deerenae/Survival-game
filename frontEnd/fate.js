1ac556c25937601303ff4bb5e9e8e451a9708435
Katie Belanger <belangeerkat@Katies-MacBook-Air.local>
2020-03-27 13:45:58 -0400

    added fate functionality and changed some ratings

--- frontEnd/fate.js
+++ frontEnd/fate.js
@@ -1,3 +1,12 @@
-fetch('http://localhost:3000/survivors')
-.then(response => response.json)    
-.then(result => console.log(result))
+const getShelter= localStorage.getItem("shelter")
+const getTool = localStorage.getItem("tool")
+const shelterValue = parseInt(getShelter)
+const toolValue = parseInt(getTool)
+    console.log(shelterValue)
+    console.log(toolValue)
+    console.log(shelterValue + toolValue > 14)
+ if (shelterValue + toolValue > 12) {
+    document.getElementById("your-fate").innerHTML += "<p>YOU SURVIVED 2020!</p>";
+ } else {
+        document.getElementById("your-fate").innerHTML += "<p>YOU DID NOT SURVIVE 2020 ;(</p>";
+}
